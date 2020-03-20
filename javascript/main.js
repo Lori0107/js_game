0a184eb24e863c5c4994e979fb2645aa33567b2b
Lori0107 <lori.benzemra@gmail.com>
2020-03-20 15:54:27 +0100

    Refactoring for positionning Player 2 + Add main.js to instanciate game's items

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+// Players's instanciation
+const player1 = new Player("Player1", 100);
+const player2 = new Player("Player2", 100);
+
+// Weapons's instanciation
+const broom = new Weapon("Broom", 10);
+const voodoo = new Weapon("Voodoo", 20);
+const sword = new Weapon("Sword", 30);
+const wand = new Weapon("Wand", 40);
--- javascript/map.js
+++ javascript/map.js
@@ -4,7 +4,6 @@ class Map {
     this.width = width,
     this.mapArray = [],
     this.forbiddenCases = [],
-    this.forbiddenPlayerCases = [],
     this.weaponsArray = []
   }
 
@@ -41,7 +40,7 @@ class Map {
     if (this.forbiddenCases.some(forbiddenItem => val.positionY === forbiddenItem.positionY && val.positionX === forbiddenItem.positionX)) {
       return this.generateItemPosition(item, isRegistered);
     } else if (isRegistered == true) {
-      item.position = [val.positionY, val.positionX];
+      item.position = {positionY: val.positionY, positionX: val.positionX};
       this.mapArray[val.positionY][val.positionX] = item;
       this.mapArray[val.positionY][val.positionX].state = item.name;
       this.forbiddenCases.push(val);
@@ -52,68 +51,43 @@ class Map {
   }
 
   generateWeapons = () => {
-    const broom = new Weapon("Broom", 10);
-    const voodoo = new Weapon("Voodoo", 20);
-    const sword = new Weapon("Sword", 30);
-    const wand = new Weapon("Wand", 40);
-
     this.weaponsArray.push(broom, voodoo, sword, wand);
     this.weaponsArray.forEach(weapon => this.generateItemPosition(weapon, true));
   }
 
   generatePlayers = () => {
-    const player1 = new Player("Player1", 100);
-    const player2 = new Player("Player2", 100);
-    let positionPlayer1 = this.generateItemPosition(player1, true);
-    this.forbiddenPlayerCases.push(positionPlayer1);
-    this.generatePlayer2Position(player2, this.forbiddenPlayerCases);
-    player1.weapon = this.weaponsArray[0];
-    player1.weapon.position = player1.position;
-    player2.weapon = this.weaponsArray[0];
+    this.generateItemPosition(player1, true);
+    this.generatePlayer2Position(player2);
+    
+    player1.weapon = broom;
+    player2.weapon = broom;
   }
 
-  generatePlayer2Position = (playerItem, forbiddenCasesItem) => {
-    let val = this.generateItemPosition(playerItem, false);
-    if (val.positionX === forbiddenCasesItem[0].positionX && val.positionY === forbiddenCasesItem[0].positionY || 
-      val.positionX === forbiddenCasesItem[0].positionX + 1 && val.positionY === forbiddenCasesItem[0].positionY ||
-      val.positionX === forbiddenCasesItem[0].positionX - 1  && val.positionY === forbiddenCasesItem[0].positionY ||
-      val.positionX === forbiddenCasesItem[0].positionX  && val.positionY === forbiddenCasesItem[0].positionY + 1 ||
-      val.positionX === forbiddenCasesItem[0].positionX  && val.positionY === forbiddenCasesItem[0].positionY - 1 ||
-      val.positionX === forbiddenCasesItem[0].positionX + 1 && val.positionY === forbiddenCasesItem[0].positionY + 1 ||
-      val.positionX === forbiddenCasesItem[0].positionX - 1 && val.positionY === forbiddenCasesItem[0].positionY - 1 ||
-      val.positionX === forbiddenCasesItem[0].positionX + 1 && val.positionY === forbiddenCasesItem[0].positionY - 1 ||
-      val.positionX === forbiddenCasesItem[0].positionX - 1 && val.positionY === forbiddenCasesItem[0].positionY + 1 
-    ) {
-      return this.generatePlayer2Position(playerItem, forbiddenCasesItem);
+  comparePlayersPosition = (playerOne, playerTwo) => {
+    if (playerOne.positionX === playerTwo.positionX && playerOne.positionY === playerTwo.positionY || 
+        playerOne.positionX === playerTwo.positionX + 1 && playerOne.positionY === playerTwo.positionY ||
+        playerOne.positionX === playerTwo.positionX - 1  && playerOne.positionY === playerTwo.positionY ||
+        playerOne.positionX === playerTwo.positionX  && playerOne.positionY === playerTwo.positionY + 1 ||
+        playerOne.positionX === playerTwo.positionX  && playerOne.positionY === playerTwo.positionY - 1 ||
+        playerOne.positionX === playerTwo.positionX + 1 && playerOne.positionY === playerTwo.positionY + 1 ||
+        playerOne.positionX === playerTwo.positionX - 1 && playerOne.positionY === playerTwo.positionY - 1 ||
+        playerOne.positionX === playerTwo.positionX + 1 && playerOne.positionY === playerTwo.positionY - 1 ||
+        playerOne.positionX === playerTwo.positionX - 1 && playerOne.positionY === playerTwo.positionY + 1) {
+      return true;
+    } else return false;
+  }
+
+  generatePlayer2Position = (playerItem) => {
+    let itemPosition = this.generateItemPosition(playerItem, false);
+    if (this.comparePlayersPosition(player1.position, itemPosition)) {
+      return this.generatePlayer2Position(playerItem);
     } else {
-      playerItem.position = [val.positionY, val.positionX];
-      this.mapArray[val.positionY][val.positionX] = playerItem;
-      this.mapArray[val.positionY][val.positionX].state = playerItem.name;
-      this.forbiddenCases.push(val);
+      playerItem.position = {positionY: itemPosition.positionY, positionX: itemPosition.positionX};
+      this.mapArray[itemPosition.positionY][itemPosition.positionX] = playerItem;
+      this.mapArray[itemPosition.positionY][itemPosition.positionX].state = playerItem.name;
+      this.forbiddenCases.push(itemPosition);
+    }
   }
-
-    // const test = this.generateItemPosition(playerItem, false);
-    // do {
-    //   return this.generatePlayer2Position(playerItem, forbiddenCasesItem)
-    // } while (test.positionX === forbiddenCasesItem.positionX && test.positionY === forbiddenCasesItem.positionY || 
-    //     test.positionX === forbiddenCasesItem.positionX + 1 && test.positionY === forbiddenCasesItem.positionY ||
-    //     test.positionX === forbiddenCasesItem.positionX - 1  && test.positionY === forbiddenCasesItem.positionY ||
-    //     test.positionX === forbiddenCasesItem.positionX  && test.positionY === forbiddenCasesItem.positionY + 1 ||
-    //     test.positionX === forbiddenCasesItem.positionX  && test.positionY === forbiddenCasesItem.positionY - 1 ||
-    //     test.positionX === forbiddenCasesItem.positionX + 1 && test.positionY === forbiddenCasesItem.positionY + 1 ||
-    //     test.positionX === forbiddenCasesItem.positionX - 1 && test.positionY === forbiddenCasesItem.positionY - 1 ||
-    //     test.positionX === forbiddenCasesItem.positionX + 1 && test.positionY === forbiddenCasesItem.positionY - 1 ||
-    //     test.positionX === forbiddenCasesItem.positionX - 1 && test.positionY === forbiddenCasesItem.positionY + 1 
-    //   );
-    //   this.mapArray[test.positionY][test.positionX] = playerItem;
-    //   this.mapArray[test.positionY][test.positionX].state = playerItem.name;
-    //   console.log("--------------- Player 2 position pushed ---------------");
-    //   console.log(test);
-    //   this.forbiddenCases.push(test);
-  }
-
-  // assignWeaponToPlayer = (player, weapons) => {
-  // };
 
   displayItems = (map, customClass) => {
     $(map).append('<div class="'+ customClass +'"></div>');
@@ -128,7 +102,7 @@ class Map {
   }
 };
 
-let myMap = new Map(10, 10);
+const myMap = new Map(10, 10);
 myMap.generateMap();
 myMap.generateDisabledCases(7);
 myMap.generateWeapons();
